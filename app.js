8cdcfe6accfd4e567d6667d53580bd1100f984fd
Santosh Kumar <santosh3052000@gmail.com>
2025-10-04 18:50:16 +0530

    morgen comment again

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(morgan('dev'))
+//app.use(morgan('dev'))
 app.use('/',Router)
 
 app.listen(1000,()=>{console.log(`Server is running at http://localhost:1000`)})
